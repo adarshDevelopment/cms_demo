1e6d1300ef175788fa2836e2b6edb981870a3bff
adarshDevelopment <152478141+adarshDevelopment@users.noreply.github.com>
2024-07-20 21:40:44 +0545

    form added

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -85,10 +85,12 @@
 
         {{-- Main content --}}
         <div
-            class="lg:ml-[300px] max:w-screen flex-grow bg-indigo-100 flex flex-col max-w-screen \\ justify-between min-h-screen">
+            class="lg:ml-[300px] bg-indigo-100 max:w-screen flex-grow  flex flex-col max-w-screen justify-between min-h-screen px-3">
 
             {{-- top items --}}
             <div class="flex flex-col gap-5">
+
+
 
                 {{-- top nav items --}}
                 <div class="flex justify-between items-center h-16">
@@ -105,19 +107,31 @@
 
 
                     {{-- top right items --}}
-                    <div class="flex gap-3">
+                    <div class="flex gap-5 items-center">
+
+                        <button>
+                            <i class="fa-regular fa-bell text-xl"></i>
+
+                        </button>
+
+
+
+                        {{-- <div class="h-12 w-[2px] bg-gray-400 my-auto rounded-md">
+                        </div> --}}
+
                         {{-- log out buttons --}}
                         <div class="flex items-center gap-5 overflow-hidden">
 
                             <div class="overflow-hidden rounded-full cursor-pointer ">
                                 <img src="https://t3.ftcdn.net/jpg/02/22/85/16/360_F_222851624_jfoMGbJxwRi5AWGdPgXKSABMnzCQo9RN.jpg"
-                                    class="h-14 w-14 overflow-hidden rounded-full " alt="">
+                                    class="h-11 w-12 overflow-hidden rounded-full " alt="">
                             </div>
 
 
-
+                            <div class="h-12 w-[2px] bg-gray-400 my-auto rounded-md">
+                            </div>
                             {{-- <span>Logout</span> --}}
-                            <button class="text-xl ">
+                            <button class="text-md font-semibold text-gray-700 ">
                                 Log Out
                                 {{-- <i class="fa-solid fa-right-from-bracket text-2xl"></i> --}}
                             </button>
@@ -131,16 +145,110 @@
 
 
                 {{-- Center Items --}}
-                <div class="grid lg:grid-cols-4 gap-2 md:grid-cols-2">
-                    <div class=" w-full bg-amber-400 mx-auto py-5 text-center">hello</div>
-                    <div class=" w-full bg-amber-400 mx-auto py-5 text-center">hello</div>
-                    <div class=" w-full bg-amber-400 mx-auto py-5 text-center">hello</div>
-                    <div class=" w-full bg-amber-400 mx-auto py-5 text-center">hello</div>
-                    <div class=" w-full bg-amber-400 mx-auto py-5 text-center">hello</div>
-                    <div class=" w-full bg-amber-400 mx-auto py-5 text-center">hello</div>
-                    <div class=" w-full bg-amber-400 mx-auto py-5 text-center">hello</div>
-                    <div class=" w-full bg-amber-400 mx-auto py-5 text-center">hello</div>
+
+                {{-- left side --}}
+                <div class="bg-white p-5 shadow-2xl rounded-md">
+                    <form action="" class="">
+
+                        <div
+                            class=" w-[80%] mx-auto grid lg:grid-cols-3 gap-x-24 gap-y-11 md:grid-cols-2 text-gray-700 text-xl  pb-10">
+                            <div class="col-span-3 bg-gray-300 py-2 px-3 rounded-sm text-center">
+                                Please make sure you only fill the correct information.
+                            </div>
+
+                            {{-- first second rows --}}
+                            <div class="row-span-2 col-span-1">
+                                <span class="text-semibold">Personal Information</span>
+                                <p class="text-sm">Information about the section could go here and a brief description
+                                    of how this might be used.
+                                </p>
+                            </div>
+
+                            <div class="col-span-1 flex flex-col gap-5">
+                                <label for="name" class="col-span-1">First Name</label>
+                                <input type="text"
+                                    class="= py-1 px-3 rounded-sm border-gray-500 border border-solid focus:border-red-400">
+                            </div>
+
+                            <div class="col-span-1 flex flex-col gap-5">
+                                <label for="name" class="col-span-1">Last Name</label>
+                                <input type="text"
+                                    class="= py-1 px-3 rounded-sm border-gray-500 border border-solid focus:border-red-400">
+                            </div>
+                            <div class="col-span-1 flex flex-col gap-5">
+                                <label for="name" class="col-span-1">Email Address</label>
+                                <input type="text"
+                                    class="= py-1 px-3 rounded-sm border-gray-500 border border-solid focus:border-red-400">
+                            </div>
+
+                            <div class="col-span-1 flex flex-col gap-5">
+                                <label for="name" class="col-span-1">Phone number</label>
+                                <input type="text"
+                                    class="= py-1 px-3 rounded-sm border-gray-500 border border-solid focus:border-red-400">
+                            </div>
+
+
+                            <hr class="col-span-3">
+
+                            <div class="row-span-2 col-span-1">
+                                <span class="text-semibold">Security</span>
+                                <p class="text-sm">Information about the section could
+                                    go here and a brief description of how thins might be used.
+                                </p>
+                            </div>
+
+                            <div class="col-span-1 flex flex-col gap-5">
+                                <label for="name" class="col-span-1">Password</label>
+                                <input type="text"
+                                    class="= py-1 px-3 rounded-sm border-gray-500 border border-solid focus:border-red-400">
+                            </div>
+
+                            <div class="col-span-1 flex flex-col gap-5">
+                                <label for="name" class="col-span-1">Security Code</label>
+                                <input type="text"
+                                    class="= py-1 px-3 rounded-sm border-gray-500 border border-solid focus:border-red-400">
+                            </div>
+
+                            <div class="col-span-1 flex flex-col gap-5">
+                                <label for="name" class="col-span-1">Recovery Email address</label>
+                                <input type="text"
+                                    class="= py-1 px-3 rounded-sm border-gray-500 border border-solid focus:border-red-400">
+                            </div>
+
+                            <div class="col-span-1 flex flex-col gap-5">
+                                <label for="name" class="col-span-1">Alternate phone number</label>
+                                <input type="text"
+                                    class="= py-1 px-3 rounded-sm border-gray-500 border border-solid focus:border-red-400">
+                            </div>
+
+
+                            <div class="col-span-3 flex justify-start">
+                                <button class="bg-blue-500 py-2 px-5 rounded-md text-white hover:bg-blue-600 w-full">
+                                    Submit
+                                </button>
+                            </div>
+
+
+
+                        </div>
+
+
+                    </form>
                 </div>
+
+
+
+
+
+                {{-- <div class=" w-full bg-amber-400 mx-auto py-5 text-center">hello</div>
+                    <div class=" w-full bg-amber-400 mx-auto py-5 text-center">hello</div>
+                    <div class=" w-full bg-amber-400 mx-auto py-5 text-center">hello</div>
+                    <div class=" w-full bg-amber-400 mx-auto py-5 text-center">hello</div>
+                    <div class=" w-full bg-amber-400 mx-auto py-5 text-center">hello</div>
+                    <div class=" w-full bg-amber-400 mx-auto py-5 text-center">hello</div>
+                    <div class=" w-full bg-amber-400 mx-auto py-5 text-center">hello</div>
+                    <div class=" w-full bg-amber-400 mx-auto py-5 text-center">hello</div> --}}
+
 
 
             </div>
